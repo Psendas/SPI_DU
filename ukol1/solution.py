02715a572d30cd077c1a26252bf900383ee7a86b
Tomáš Pšenička <psenito1@fit.cvut.cz>
2020-05-01 04:48:45 +0200

    Task1 report done, needs validation.

--- ukol1/solution.py
+++ ukol1/solution.py
@@ -40,26 +40,38 @@ def char_prob(filename):
         for c, v in chars.items():
             chars[c] = v / char_count
 
-        print("Char\t|  Probability")
-        print("-----------------------------")
         sorted_chars = sorted(chars, key=chars.get, reverse=True)
         sorted_values = []
         for c in sorted_chars:
             sorted_values.append(chars[c])
-            print(f'"{c}"\t| {chars[c]}')
 
         ent = entropy(sorted_values)
         print(f'Chars entropy: {ent}')
 
         plt.bar(sorted_chars, sorted_values, width=0.5, color='g')
-        plt.title("Grafické znázornění pravděpodobností znaků textu")
         plt.xlabel("Znaky výchozího textu")
         plt.ylabel("Pravděpodobnost")
-        save_to = filename.split('//')[2]
-        save_to = save_to.split('.')[0]
+        file_set = filename.split('//')[2]
+        save_to = file_set.split('.')[0]
         plt.savefig(save_to + "_char_prob.png", dpi=350)
 
-        huf_code(sorted_chars, sorted_values)
+        h_code = huf_code(sorted_chars, sorted_values)
+
+        print("\\begin{table}[!ht]")
+        print("\\centering")
+        print("\\begin{tabular}{ | c | l | r | } \\hline")
+        print("Znak\t&\tPravděpodobnost\t&\tKód\t\\\\ \\hline")
+        for c in sorted_chars:
+            if c == '\\n':
+                print(f'"\\textbackslash n"\t&\t{chars[c]}\t&\t{h_code[c]}\t\\\\ \\hline')
+            elif c == '␣':
+                print(f'"\\textvisiblespace "\t&\t{chars[c]}\t&\t{h_code[c]}\t\\\\ \\hline')
+            else:
+                print(f'"{c}"\t&\t{chars[c]}\t&\t{h_code[c]}\t\\\\ \\hline')
+        print("\\end{tabular}")
+        print("\\caption{Pravděpodobnosti znaků a Huffmanův kód sada %s.}" % file_set)
+        print("\\label{pzhk_%s_table}" % file_set)
+        print("\\end{table}")
 
 class Node:
     def __init__(self, char='', prob=0):
@@ -81,9 +93,14 @@ def show_code(node, chars, h_code):
 def code_len(code, chars, probs):
     sum = 0
     for char, c in code.items():
-        index = chars.index(char)
-        sum += len(c) * probs[index]
+        try:
+            index = chars.index(char)
+            sum += len(c) * probs[index]
+        except ValueError:
+            pass
     return sum
+
+CC_code = None
 
 def huf_code(chars, probs):
     que = []
@@ -104,18 +121,27 @@ def huf_code(chars, probs):
 
     h_code = dict()
     show_code(root, "", h_code)
-    sorted_code_chars = sorted(h_code, key=lambda k: h_code[k])
-    sorted_code_chars = sorted(sorted_code_chars, key=lambda k: len(h_code[k]))
 
-    for char in sorted_code_chars:
-        print(char + " : " + h_code[char])
+    # sorted_code_chars = sorted(h_code, key=lambda k: h_code[k])
+    # sorted_code_chars = sorted(sorted_code_chars, key=lambda k: len(h_code[k]))
+    # for char in sorted_code_chars:
+    #     print(char + " : " + h_code[char])
 
-    c_len = code_len(h_code, chars, probs)
-    print(f'Code len = {c_len}')
+    global CC_code
+    if CC_code is None:
+        c_len = code_len(h_code, chars, probs)
+        print(f'Code len = {c_len}')
+        CC_code = h_code
+    else:
+        c_len = code_len(CC_code, chars, probs)
+        print(f'First code len for this text: {c_len}')
+
+
+    return h_code
 
 if __name__ == "__main__":
 
-    char_prob(folder+xxx)
     char_prob(folder+yyy)
+    char_prob(folder+xxx)
 
 
